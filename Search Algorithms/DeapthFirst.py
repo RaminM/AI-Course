689ef0f730eec8ba328a2ef29398657861a540b7
Seyedramin Mortazavi <ramin.rme@gmail.com>
2022-03-17 21:46:32 +0100

    Empty DFS

---
+++ Search Algorithms/DeapthFirst.py
@@ -0,0 +1,8 @@
+class DepthFirstSearch:
+    def __init__(self,graph,start,end):
+        self.graph = graph
+        self.start = start
+        self.end = end
+    
+    def calculate_dfs(self):
+        pass
